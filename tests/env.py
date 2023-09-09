a25869a3994d0ccd06a929ee50ad8d15f9173fd9
Raphael2b3 <raphael.schuetz0311@gmail.com>
2023-09-09 05:39:08 +0200

    implemented cicd and configured file system

---
+++ tests/env.py
@@ -0,0 +1,16 @@
+import sys
+
+from dotenv import dotenv_values
+
+config = dotenv_values("./.env")
+
+HOST = config["HOST"]
+PORT = int(config["PORT"])
+
+DB_CONNECTION_STRING = config["DB_CONNECTION_STRING"]
+DB_USERNAME = config["DB_USERNAME"]
+DB_PASSWORD = config["DB_PASSWORD"]
+
+__SECRET_KEY = config["__SECRET_KEY"]
+ALGORITHM = config["ALGORITHM"]
+ACCESS_TOKEN_EXPIRE_MINUTES = int(config["ACCESS_TOKEN_EXPIRE_MINUTES"])
